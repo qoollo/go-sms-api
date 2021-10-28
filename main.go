ae6be8818146a2e9d18c7d4eb808c45efd9b6a9f
Nikolay <varlamow.col@yahoo.com>
2021-10-28 14:16:28 +0300

    read all test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/minish144/go-sms-api/modules/modem"
 	"github.com/minish144/go-sms-api/modules/server"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -18,6 +19,23 @@ func main() {
 		}).Fatalln()
 	}
 	logrus.Info("viper config initialized successfully")
+
+	go func() {
+		comport := viper.GetString("modem.comport")
+		baudrate := viper.GetInt("modem.baudrate")
+		newModem, err := modem.New(comport, baudrate)
+		if err != nil {
+			logrus.WithFields(
+				logrus.Fields{
+					"error":    err.Error(),
+					"comport":  comport,
+					"baudrate": baudrate,
+				},
+			).Errorln("failed to initialize a modem")
+			return
+		}
+		newModem.ReadAll()
+	}()
 
 	// server init
 	server.Run()
